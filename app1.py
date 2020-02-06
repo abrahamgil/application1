27567a359643058552161376d32c3b71d3dff52d
Abraham <abrahamgil2@gmail.com>
2020-02-06 14:39:26 -0600

    Added user interaction to see if they really want that word

--- app1.py
+++ app1.py
@@ -8,7 +8,12 @@ def lookup(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w,data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w,data.keys())[0]
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w,data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        else:
+            return "The word doesn't exist. Please try again."
+
     else:
         return "The word doesn't exist. Please try again."
 
